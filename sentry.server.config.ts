d003e114aa17f5fb8b9817203429c906949fd216
Vempali Hrishita <vempalihrishita05@gmail.com>
2025-11-21 08:38:09 +0530

    Update sentry.server.config.ts

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -3,7 +3,6 @@ Sentry.init({
   dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
